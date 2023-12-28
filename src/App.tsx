434e4b244ee5add52cfe9ac344e202ab11cab115
Ben <22252563+ben-732@users.noreply.github.com>
2023-12-28 20:33:05 +1300

    New Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-
-import Landing from "./Landing";
-import Projects from "./Projects";
-import Skills from "./Skills";
-import NavBar from "./NavBar";
-import About from "./About";
+import Card from "./components/Card";
 
 function App() {
   return (
+    <div className="w-full h-screen overflow-x-hidden scroll-smooth pageBackground">
+      <div className="header m-20">
+        <span className="text-lg text-white ">Hello, I am</span>
+        <h1 className="text-7xl font-extrabold text-white pt-0">
+          Ben <br></br>McMurtrie
+        </h1>
+        <div className="flex flex-col m-2 text-gray-200 leading-tight	">
+          <span>Web Developer</span>
+          <span>Student at the University of Auckland</span>
+          <span>Software Engineering Intern at Tidy International</span>
+        </div>
+      </div>
+
     <div
       className="w-full bg-cover overflow-x-hidden scroll-smooth"
       // style={{ backgroundImage: `url(${background_img})` }}
